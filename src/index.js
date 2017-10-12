a47869872fe095faf74f0531cfdde70a08f5b856
yuhao.ju <yuhao.ju@qunar.com>
2017-10-12 15:56:27 +0800

    feat: 定义 getPages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import Home from './home';
 import About from './about';
 
-export const pages = [
-    {
-        path: '/',
-        component: Home
-    },
-    {
-        path: '/about',
-        component: About
-    }
-]
+export function getPages() {
+    // const { data: posts } = await axios.get('http://jsonplaceholder.typicode.com/posts')
+    return [
+        {
+            path: '/',
+            component: Home
+        }, {
+            path: '/about',
+            component: About,
+            getProps: () => ({posts})
+        }
+    ]
+}
--- ssr/share/index.js
+++ ssr/share/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import {pages} from '../../src/index.js'
+import { getPages } from '../../src/index.js'
 
+const pages = getPages();
 export default class App extends React.Component {
     constructor(props) {
         super(props);
