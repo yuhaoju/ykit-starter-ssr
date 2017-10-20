f62ec3fe4abe159086ec02e5ed48a49acec42b4f
yuhao.ju <yuhao.ju@qunar.com>
2017-10-20 19:43:16 +0800

    chore: upgrade ykit-config-ssr

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -26,22 +26,6 @@ const Home = (props) => {
 const styles = StyleSheet.create({
     red: {
         backgroundColor: 'red'
-    },
-
-    blue: {
-        backgroundColor: 'blue'
-    },
-
-    hover: {
-        ':hover': {
-            backgroundColor: 'red'
-        }
-    },
-
-    small: {
-        '@media (max-width: 600px)': {
-            backgroundColor: 'red',
-        }
     }
 });
 
